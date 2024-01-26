6fe2ad801fd0c717661a65f0b0d7e507f4ce060a
ChoMihyang <chomh316@gmail.com>
2024-01-27 03:06:33 +0900

    init database

--- back/main.py
+++ back/main.py
@@ -1,12 +1,31 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+import pymysql
+
 
 app = FastAPI()
+load_dotenv()
 
+def mysql_create_session():
+    conn = pymysql.connect(
+       host=os.getenv("DB_URL"), 
+       user=os.getenv("USER"), 
+       password=os.getenv("PASSWORD"), 
+       db=os.getenv("DB_NAME"), 
+       charset="utf8", 
+       cursorclass=pymysql.cursors.DictCursor
+    )
+    cur = conn.cursor()
+    return conn, cur
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/mihyang")
-async def root():
-    return {"message": "mihyang"}
+@app.get("/staffInfo")
+def getStaffInfoByName():
+  conn, cur = mysql_create_session() 
+  try:
+    sql = "SELECT * FROM staff"
+    cur.execute(sql)
+    row = cur.fetchone()
+  finally:
+    conn.close()
+  return row
