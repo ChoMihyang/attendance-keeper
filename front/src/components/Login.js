b3107301c04942e8708252a49def158b568f580f
ChoMihyang <chomh316@gmail.com>
2024-02-02 02:32:56 +0900

    Feat login Page

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,21 +1,53 @@
-import React, { useState } from "react";
-import { Container, TextField, Button } from "@mui/material";
+    import React, { useState } from "react";
+    import {
+    Container,
+    TextField,
+    Button,
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    InputAdornment,
+    IconButton,
+    } from "@mui/material";
+    import { Visibility, VisibilityOff } from "@mui/icons-material";
 
-function Login() {
+    function Login() {
     const [staffId, setStaffId] = useState("");
     const [password, setPassword] = useState("");
 
     const handleChange = (event) => {
         if (event.target.name === "staffId") {
-        setStaffId(event.target.value);
+        setStaffId(parseInt(event.target.value));
         } else if (event.target.name === "password") {
         setPassword(event.target.value);
         }
-        console.log(staffId, password);
+        console.log(typeof(staffId), password);
     };
-    const handleSubmit = (event) => {
-        console.log(event);
-    }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            const response = await fetch('http://localhost:8000/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ staffId: staffId, password: password })
+            });
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+    
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
 
     return (
         <Container maxWidth="sm">
@@ -33,15 +65,32 @@ function Login() {
                 sx={{ width: "20rem", my: "0.5rem" }}
             />
             <br />
-            <TextField
-                id="outlined-basic"
+            <FormControl sx={{ width: "20rem" }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">
+                Password
+                </InputLabel>
+                <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                    <InputAdornment position="end">
+                    <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                    >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                    </InputAdornment>
+                }
+                label="Password"
                 name="password"
                 value={password}
                 onChange={handleChange}
-                label="Password"
                 variant="outlined"
-                sx={{ width: "20rem", my: "0.5rem" }}
-            />
+                />
+            </FormControl>
             <br />
             <Button
                 type="submit"
@@ -55,6 +104,6 @@ function Login() {
         </div>
         </Container>
     );
-}
+    }
 
 export default Login;
